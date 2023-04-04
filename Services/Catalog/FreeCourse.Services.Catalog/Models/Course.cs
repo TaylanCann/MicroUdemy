d4a75937f38ac4014a7889d5f552ef6205414d7a
TaylanCann <taylan.hardal@atmosware.turkcell.com.tr>
2023-04-04 22:19:03 +0300

    Models added

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Description { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+        
+        [BsonRepresentation(BsonType.ObjectId)]
+        public Feature Feature { get; set; }
+    }
+}
