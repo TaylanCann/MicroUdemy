9131bc63639705270807e1db109e90c10dbec440
TaylanCann <taylancanh@gmail.com>
2023-04-06 23:48:08 +0300

    GetAllAsync

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
 using FreeCourse.Services.Catalog.Models;
 using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.Dtos;
 using MongoDB.Driver;
 
 namespace FreeCourse.Services.Catalog.Services
@@ -8,6 +10,7 @@ namespace FreeCourse.Services.Catalog.Services
      class CourseService
     {
         private readonly IMongoCollection<Course> _courseCollection;
+        private readonly IMongoCollection<Category> _categoryCollection;
         private readonly IMapper _mapper;
 
         public CourseService(IMapper mapper, IDatabaseSettings databaseSettings)
@@ -15,8 +18,25 @@ namespace FreeCourse.Services.Catalog.Services
             var client = new MongoClient(databaseSettings.ConnectionString);
             var database = client.GetDatabase(databaseSettings.DatabaseName);
 
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
             _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
             _mapper = mapper;
         }
+
+
+        public async Task<Response<List<CourseDto>>> GetAllAsync()
+        {
+            var courses = await _courseCollection.Find(course => true).ToListAsync();
+
+            if (courses.Any())
+            {
+
+            }
+
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+        }
+
+
+
     }
 }
