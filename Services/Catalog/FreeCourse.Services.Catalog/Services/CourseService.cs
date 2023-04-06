ad30289e12a22660f3ea244461fe53a2902820ec
TaylanCann <taylancanh@gmail.com>
2023-04-06 23:33:24 +0300

    Add Course Service

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Services.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+     class CourseService
+    {
+        private readonly IMongoCollection<Course> _courseCollection;
+        private readonly IMapper _mapper;
+
+        public CourseService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _mapper = mapper;
+        }
+    }
+}
