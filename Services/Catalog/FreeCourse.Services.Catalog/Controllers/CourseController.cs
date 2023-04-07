5113759835862ccf11199fc0e0bc3f2b2bd0078f
TaylanCann <taylancanh@gmail.com>
2023-04-08 01:31:05 +0300

    CustomBaseController Added

--- Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FreeCourse.Services.Catalog.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FreeCourse.Services.Catalog.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CourseController : ControllerBase
+     class CourseController : ControllerBase
     {
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
     }
 }
