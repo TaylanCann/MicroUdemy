cae460b803e4416336f0537a4114250f251b17de
TaylanCann <taylancanh@gmail.com>
2023-04-10 02:45:39 +0300

    Id Nullable Fix

--- Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseCreateDto.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseCreateDto.cs
@@ -2,7 +2,7 @@
 {
     public class CourseCreateDto
     {
-        public string Id { get; set; }
+        public string? Id { get; set; }
         public string Name { get; set; }
         public int Description { get; set; }
 
