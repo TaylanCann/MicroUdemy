feb85abac09725509ebf71d79d2b8031addff4ff
TaylanCann <taylancanh@gmail.com>
2023-04-13 23:59:04 +0300

    Regular

--- Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseUpdateDto.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseUpdateDto.cs
@@ -5,13 +5,10 @@
         public string? Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
         public decimal Price { get; set; }
         public string UserId { get; set; }
-
         public string Picture { get; set; }
         public FeatureDto Feature { get; set; }
-
         public string CategoryId { get; set; }
     }
 }
