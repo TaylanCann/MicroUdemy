ebebc0568c584956a67cc1ecdc552c190ff2ee56
TaylanCann <taylancanh@gmail.com>
2023-04-16 16:14:28 +0300

    AddUserController

---
+++ IdentityServer/FreeCourse.IdentityServer/Controller/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.IdentityServer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
