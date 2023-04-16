a0aed0db907a3ee17e2ce75b8091acdd61fba08d
TaylanCann <taylancanh@gmail.com>
2023-04-17 01:31:42 +0300

    UserController ok

--- IdentityServer/FreeCourse.IdentityServer/Controller/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controller/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using FreeCourse.IdentityServer.Dto;
 using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FreeCourse.IdentityServer.Controller
@@ -33,8 +35,9 @@ namespace FreeCourse.IdentityServer.Controller
 
             if (!result.Succeeded)
             {
-                return BadRequest(Response<>);
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(),400));
             }
+            return NoContent();
 
         }
     }
