14bc442c70202fa2a1b372dcd631a0a7d86863a3
TaylanCann <taylancanh@gmail.com>
2023-04-16 19:17:36 +0300

    SingUp fucn

--- IdentityServer/FreeCourse.IdentityServer/Controller/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controller/UserController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FreeCourse.IdentityServer.Dto;
+using FreeCourse.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Threading.Tasks;
 
 namespace FreeCourse.IdentityServer.Controller
 {
@@ -7,5 +12,30 @@ namespace FreeCourse.IdentityServer.Controller
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SingUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City,
+            };
+            
+            var result = await  _userManager.CreateAsync(user,signupDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<>);
+            }
+
+        }
     }
 }
