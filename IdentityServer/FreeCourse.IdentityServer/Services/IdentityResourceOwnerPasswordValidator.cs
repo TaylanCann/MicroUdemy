aaf990bffdd816634811b52f2b40ea03315690c6
TaylanCann <taylancanh@gmail.com>
2023-04-24 23:39:24 +0300

    validateOk

--- IdentityServer/FreeCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
+++ IdentityServer/FreeCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FreeCourse.IdentityServer.Models;
+using IdentityModel;
 using IdentityServer4.Validation;
 using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
@@ -23,7 +24,20 @@ namespace FreeCourse.IdentityServer.Services
             {
                var errors = new Dictionary<string, object>();
                 errors.Add("errors", new List<string> { "Kullanıcı adı veya şifre yanlış" });
+                context.Result.CustomResponse = errors;
+                return;
             }
+            var passwordCheck = await _userManager.CheckPasswordAsync(existUser, context.Password);
+            
+            if(!passwordCheck)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors", new List<string> { "Kullanıcı adı veya şifre yanlış" });
+                context.Result.CustomResponse = errors;
+                return;
+            }
+
+            context.Result = new GrantValidationResult(existUser.Id.ToString(), OidcConstants.AuthenticationMethods.Password);
 
         }
     }
