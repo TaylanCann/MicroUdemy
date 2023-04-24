fd963e588c64410eccdee1897f208476226961e1
TaylanCann <taylancanh@gmail.com>
2023-04-24 23:28:00 +0300

    add IResource Interface

---
+++ IdentityServer/FreeCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,13 @@
+﻿using IdentityServer4.Validation;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
