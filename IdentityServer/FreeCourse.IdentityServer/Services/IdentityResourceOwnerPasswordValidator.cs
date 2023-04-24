d5d8dfc2930c42dba828ca72e6fc270f10e5f6e1
TaylanCann <taylancanh@gmail.com>
2023-04-24 23:35:38 +0300

    add error

--- IdentityServer/FreeCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
+++ IdentityServer/FreeCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -1,13 +1,30 @@
-﻿using IdentityServer4.Validation;
+﻿using FreeCourse.IdentityServer.Models;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace FreeCourse.IdentityServer.Services
 {
     public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
     {
-        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
         {
-            throw new System.NotImplementedException();
+            _userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var existUser = await _userManager.FindByEmailAsync(context.UserName);
+
+            if (existUser != null)
+            {
+               var errors = new Dictionary<string, object>();
+                errors.Add("errors", new List<string> { "Kullanıcı adı veya şifre yanlış" });
+            }
+
         }
     }
 }
