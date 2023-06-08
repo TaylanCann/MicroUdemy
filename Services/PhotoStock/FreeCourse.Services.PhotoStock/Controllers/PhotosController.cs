8ad720cf18157801e5983d4024022b4304f30263
TaylanCann <taylancanh@gmail.com>
2023-06-08 23:50:12 +0300

    add photo dto

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FreeCourse.Services.PhotoStock.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PhotosController : ControllerBase
+    public class PhotosController : CustomBaseController
     {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo !=null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using (var stream = new FileStream(path,FileMode.Create))
+                {
+                    photo.CopyToAsync(stream, cancellationToken);
+                }
+
+                var returnPath = "photos" + photo.FileName;
+            }
+        }
     }
 }
