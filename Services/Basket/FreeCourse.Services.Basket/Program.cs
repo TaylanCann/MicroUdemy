ea5ec4ca3ab1120c87c8dddb7eba97b558d17e37
TaylanCann <taylancanh@gmail.com>
2023-06-30 19:29:22 +0300

    basket service programcs

--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -33,7 +33,7 @@ var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticat
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
     options.Authority = builder.Configuration["IdentityServerURL"];
-    options.Audience = "resource_catalog";
+    options.Audience = "resource_basket";
     options.RequireHttpsMetadata = false;
 });
 
