373b7138cb899c7bdc13875c2f41f573da16d575
TaylanCann <taylancanh@gmail.com>
2024-05-30 03:10:07 +0300

    comebackUpdtExtensions

---
+++ IdentityServer/FreeCourse.IdentityServer/Migrations/20240429012657_reinitial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreeCourse.IdentityServer.Migrations
+{
+    public partial class reinitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
